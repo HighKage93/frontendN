dd4bb8415db127a803cd0ea67c336dc3aa4ccd7f
HighKage93 <workmayur862@gmail.com>
2024-12-15 16:55:46 +0530

    build fix

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
         crossorigin="anonymous"
       />
 <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+{/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
 <link href="https://fonts.googleapis.com/css2?family=Encode+Sans+Expanded:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
 
 <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps }) {
 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script src="path/to/dist/feather.js" />
+        {/* <script src="path/to/dist/feather.js" /> */}
       </Head>
       <div
         style={{
